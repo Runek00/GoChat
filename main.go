645de188cea8d3b36d1fcfd365043543e60f3b09
Runek00 <runek00@gmail.com>
2023-09-30 14:47:16 +0200

    my first template

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+)
+
+func main() {
+	fmt.Println("hello world")
+
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.Execute(w, nil)
+	}
+	http.HandleFunc("/", h1)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
