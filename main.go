521d1304a8c426f0a7236d94a187b787e3096e8d
Runek00 <runek00@gmail.com>
2023-09-30 16:47:38 +0200

    templates + htmx!

--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"text/template"
 )
 
+var Texts = make([]string, 0)
+
 func main() {
-	fmt.Println("hello world")
 
 	h1 := func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("index.html"))
-		tmpl.Execute(w, nil)
+		tmpl.Execute(w, Texts)
 	}
 	http.HandleFunc("/", h1)
+
+	h2 := func(w http.ResponseWriter, r *http.Request) {
+		txt := r.PostFormValue("in1")
+		Texts = append(Texts, txt)
+		tmpl := template.Must(template.New("t").Parse("<p> {{ . }} </p>"))
+		tmpl.Execute(w, txt)
+	}
+	http.HandleFunc("/add-txt/", h2)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
