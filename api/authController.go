104c9ff81b5ca209c6c6dbe5cca2e5fe966fdfc7
Runek00 <runek00@gmail.com>
2023-10-19 22:22:44 +0200

    naive attempt at updating a chart

--- api/authController.go
+++ api/authController.go
@@ -60,6 +60,7 @@ func createSession(w http.ResponseWriter, r *http.Request, login string) *sessio
 		http.Error(w, "No such user", http.StatusInternalServerError)
 		return nil
 	}
+	closeSessionIfExists(w, r)
 	session, err := Store.New(r, "session")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -69,6 +70,14 @@ func createSession(w http.ResponseWriter, r *http.Request, login string) *sessio
 	session.Values["userId"] = user.Id
 	session.Save(r, w)
 	return session
+}
+
+func closeSessionIfExists(w http.ResponseWriter, r *http.Request) {
+	session, _ := Store.Get(r, "session")
+	if session != nil {
+		session.Options.MaxAge = -1
+		Store.Save(r, w, session)
+	}
 }
 
 func CheckSession(w http.ResponseWriter, r *http.Request) bool {
--- api/initAll.go
+++ api/initAll.go
@@ -25,7 +25,8 @@ func Init() {
 	http.HandleFunc("/img/", img)
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/", defaultHandler)
-	http.HandleFunc("/charts", defaultHandler)
+	http.HandleFunc("/charts", chartHandler)
+	http.HandleFunc("/chart_script", chartScript)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
@@ -45,4 +46,33 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	}
 	tmpl := template.Must(template.ParseFiles("web/templates/hello.html"))
 	tmpl.Execute(w, nil)
+}
+
+func chartHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("web/templates/charts.html"))
+	tmpl.Execute(w, nil)
+}
+
+func chartScript(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`
+  chart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [55, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+  chart.update();
+`))
 }
